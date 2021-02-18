692fc728525f25a06de2bdcd4e660fab20c35cf9
jcole007 <61214328+jcole007@users.noreply.github.com>
2021-02-17 21:37:00 -0500

    P1HW1_Input_MadLid

---
+++ P1HW1_Input_MadLib.py
@@ -0,0 +1,12 @@
+# The first name is Eric.
+# The generic location is Chipotle.
+# The whole number is 12.
+# The plural noun is cars.
+first_name = input()
+generic_location = input()
+whole_number = int(input())
+plural_noun = input()
+print(first_name, 'went to', generic_location, 'to buy', \
+      whole_number, 'different types of', plural_noun)
+
+
