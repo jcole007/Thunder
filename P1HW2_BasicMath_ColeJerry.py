bde91a90f97af38470b7335f9949ee55eba10fbe
jcole007 <61214328+jcole007@users.noreply.github.com>
2021-02-22 16:08:51 -0500

    Add files via upload
    P1HW2_Basic Math

---
+++ P1HW2_BasicMath_ColeJerry.py
@@ -0,0 +1,13 @@
+#Basic Math
+#date: 2/21/2021
+#CTI-110 P1H1W2-Basic Math
+#Jerry Cole
+#Input the first value.
+#Input the Second value.
+#Calculate the total by add the two value together.
+#Calculate the total by multiplying the two value together.
+#Display the output.
+first_value = int(input('Please enter the first value '))
+second_value = int(input('Please enter the second value '))
+print('The sum of both number: ', first_value + second_value)
+print('Result of Multiplying the two numbers: ',first_value * second_value)
