96eecfdff28d7b300ca0f6657fb287d4ba509b13
jcole007 <61214328+jcole007@users.noreply.github.com>
2021-02-24 17:58:36 -0500

    Add files via upload
    P2LAB1_DivideByX

---
+++ P2LAB1_DivideByX_ColeJerry.py
@@ -0,0 +1,19 @@
+#Divide by x
+#Date 2/24/2021
+#CTI-110 P2LAB1-DivideByX
+#Jerry Cole
+#x = num
+#input the first number
+#input the second number
+#user divide by num
+#user1 divide by num
+#user2 divide by num
+#display the output
+
+
+user = int(input('Enter the first number.' ))
+num = int(input('Enter the last number.' ))
+user1 = user // num
+user2 = user1 // num
+user_num = user2 // num
+print (user1, user2, user_num)
