fcc45d57fc17ecfcca538a2f32e2a480f291d867
jcole007 <61214328+jcole007@users.noreply.github.com>
2021-03-09 13:44:24 -0500

    P2HW1_DistanceTraveled_jerryCole

---
+++ P2HW1_DistanceTraveled_JerryCole.py
@@ -0,0 +1,20 @@
+# The calculated time traveled project.
+# Date 3/4/2021
+# CTI-110 P2HW1 - DistanceTraveled
+# Jerry Cole
+# Ask user to enter the car speed
+# Ask user to enter hours traveled
+# Calculate distance ( speed x time)
+# Display the following:
+# The Speed Entered
+# The Time entered
+# The Output Display Distance Traveled
+
+speed = int(input('Enter the car speed: '))
+time = float(input('Enter the time traveled: '))
+distance = (speed * time)
+print('Distance Traveled', distance, 'miles.')
+input('Press Enter to Exit. ')
+
+
+
