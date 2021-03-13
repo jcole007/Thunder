72a047da0f6f830601bc3bffa9ea474ad3929134
jcole007 <61214328+jcole007@users.noreply.github.com>
2021-03-12 20:27:16 -0500

    Add files via upload
    P3LAB1_Smallest_number

---
+++ P3LAB1_Smallest_ColeJerry.py
@@ -0,0 +1,10 @@
+#LAB: Smallest Number
+#Input three interger
+#Store the input
+#Display the result
+
+val = int(input())
+val1 = int(input())
+val2 = int(input())
+val_num = (val, val1, val2)
+print('Smallest number is.', min(val_num))
